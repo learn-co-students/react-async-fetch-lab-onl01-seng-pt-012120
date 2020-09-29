fb1bcc9e42de9b56c3ff9f5faae4a481f86ef7c2
Charlie Pelton <charliejp0311@gmail.com>
2020-09-29 15:20:09 -0500

    Done.

--- .history/src/App_20200929151438.js
+++ .history/src/App_20200929151438.js
@@ -2,16 +2,12 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-    
 
-    componentDidMount(){
-        fetch('http://api.open-notify.org/astros.json').then(resp=>resp.json()).then(json=>console.log(json))
-    }
 
     render(){
         return(
             <div>
-                {}
+
             </div>
         )
     }
--- .history/src/App_20200929151830.js
+++ .history/src/App_20200929151830.js
@@ -2,7 +2,6 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-    
 
     componentDidMount(){
         fetch('http://api.open-notify.org/astros.json').then(resp=>resp.json()).then(json=>console.log(json))
@@ -11,7 +10,7 @@ class App extends Component {
     render(){
         return(
             <div>
-                {}
+
             </div>
         )
     }
